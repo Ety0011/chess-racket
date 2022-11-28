9c8d38c1d3633658f5d769e914c39e27e8557f9e
Ety <etienne.orio@orio.ch>
2022-11-28 17:25:36 +0100

    update on data types

--- board.rkt
+++ board.rkt
@@ -1,7 +1,64 @@
-(require racket/base)
+require 2htdp/image)
+(require htdp/universe)
 
-(define WHITE 0)
-(define BLACK 1)
+(define BLACK #f)
+(define WHITE #t)
+
+;; DATA TYPES
+A Type is one of those Strings:
+; - "king"
+; - "queen"
+; - "rook"
+; - "bishop"
+; - "knight"
+; - "pawn"
+
+; A Piece is a struct (make-piece type color) where
+;   - type      -> a String which indicates the type
+;   - color     -> Boolean where is BLACK or WHITE
+(define-struct piece [type color])
+
+
+; Definition of the white pieces
+(define WHITE_KING      (make-piece     "king"      WHITE))
+(define WHITE_QUEEN     (make-piece     "queen"     WHITE))
+(define WHITE_ROOK      (make-piece     "rook"      WHITE))
+(define WHITE_BISHOP    (make-piece     "bishop"    WHITE))
+(define WHITE_KNIGHT    (make-piece     "knight"    WHITE))
+(define WHITE_PAWN      (make-piece     "pawn"      WHITE))
+
+; Definition of the black pieces
+(define BLACK_KING      (make-piece     "king"      BLACK))
+(define BLACK_QUEEN     (make-piece     "queen"     BLACK))
+(define BLACK_ROOK      (make-piece     "rook"      BLACK))
+(define BLACK_BISHOP    (make-piece     "bishop"    BLACK))
+(define BLACK_KNIGHT    (make-piece     "knight"    BLACK))
+(define BLACK_PAWN      (make-piece     "pawn"      BLACK))
+
+(define EMPTY "empty")
+
+; Cell is one of:
+;   - Piece
+;   - EMPTY
+
+A Matrix is a Vector of Vectors of Cells
+(define CHESSBOARD_MATRIX
+  (vector
+   (vector BLACK_ROOK       BLACK_KNIGHT     BLACK_BISHOP       BLACK_QUEEN         BLACK_KING      BLACK_BISHOP        BLACK_KNIGHT        BLACK_ROOK)
+   (vector BLACK_PAWN       BLACK_PAWN       BLACK_PAWN         BLACK_PAWN          BLACK_PAWN      BLACK_PAWN          BLACK_PAWN          BLACK_PAWN)
+   (vector EMPTY            EMPTY            EMPTY              EMPTY               EMPTY           EMPTY               EMPTY               EMPTY)
+   (vector EMPTY            EMPTY            EMPTY              EMPTY               EMPTY           EMPTY               EMPTY               EMPTY)
+   (vector EMPTY            EMPTY            EMPTY              EMPTY               EMPTY           EMPTY               EMPTY               EMPTY)
+   (vector EMPTY            EMPTY            EMPTY              EMPTY               EMPTY           EMPTY               EMPTY               EMPTY)
+   (vector WHITE_PAWN       WHITE_PAWN       WHITE_PAWN         WHITE_PAWN          WHITE_PAWN      WHITE_PAWN          WHITE_PAWN          WHITE_PAWN)
+   (vector WHITE_ROOK       WHITE_KNIGHT     WHITE_BISHOP       WHITE_QUEEN         WHITE_KING      WHITE_BISHOP        WHITE_KNIGHT        WHITE_ROOK)))
+
+(define CHESSBOARD)
+
+
+
+
+
 
 (define PAWN-MOVES (list "TODO: see how to make moves"))
 (define ROOK-MOVES (list "TODO: see how to make moves"))
@@ -11,17 +68,10 @@
 (define QUEEN-MOVES (list "TODO: see how to make moves"))
 (define EMPTY-MOVES '())
 
-;; DATA TYPES
 
-;; PIECE
-; A piece is a struct with 2 members
-;   - type  -> a String which indicates the type
-;   - moves -> a List of valid moves for the piece
-;   - color -> Number - 0 WHITE - 1 BLACK
-(define-struct piece [type moves color])
 
-; CELL
-; A Cell is one of:
+; cell
+; A cell is one of:
 ; - 
 
 ;; BOARD
@@ -29,13 +79,6 @@
 ;    - cells  -> vector of 64 cells which are on the board
 ;    - canvas -> image result from the vector of cells
 (define-struct board [cells canvas])
-
-; Data Types
-; a Piece is a struct (make-piece color posn type) where
-; - color: Boolean where black is #f and white is #t
-; - posn: Posn where x is the column and y is the row
-; - type: String
-; - 
 
 
 
