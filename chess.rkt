a76b96f51ae6e9b6ae6d7df814f483c64a6ba744
leon9343 <asaroleonardo03@protonmail.com>
2022-12-01 18:30:36 +0100

    fixed knight + poop

--- chess.rkt
+++ chess.rkt
@@ -392,23 +392,28 @@
 ;(printBitBoard 134217728)
 
 
-(define KNIGHT       #b0000000000000000000010000000000000000000000000000000000000000000)
+(define KNIGHT       #b1000000000000000000000000000000000000000000000000000000000000000)
 (define NOT_WHITE    #b0000000001100000000001000001000000001100000010100000000000010010)
 (define FILE_H       #b0000000100000001000000010000000100000001000000010000000100000001)
 (define FILE_A       #b1000000010000000100000001000000010000000100000001000000010000000)
-(define FILE_GH      #b1100000011000000110000001100000011000000110000001100000011000000)
-(define FILE_AB      #b0000001100000011000000110000001100000011000000110000001100000011) 
+(define FILE_GH      #b0000001100000011000000110000001100000011000000110000001100000011)
+(define FILE_AB      #b1100000011000000110000001100000011000000110000001100000011000000) 
 
-(define (knight-moves bb)
-  ( bitwise-ior (bitwise-ior (bitwise-and (arithmetic-shift bb 17)  (bitwise-not FILE_A)) 
-                             (bitwise-and (arithmetic-shift bb 10)  (bitwise-not FILE_AB)))
-                (bitwise-ior (bitwise-and (arithmetic-shift bb -6)  (bitwise-not FILE_AB)) 
-                             (bitwise-and (arithmetic-shift bb -15) (bitwise-not FILE_A)))
-                               
-                (bitwise-ior (bitwise-ior (bitwise-and (arithmetic-shift bb 15) (bitwise-not FILE_H))
-                             (bitwise-and (arithmetic-shift bb 6)(bitwise-not FILE_GH))))
-                (bitwise-ior (bitwise-and (arithmetic-shift bb -10)(bitwise-not FILE_GH))
-                             (bitwise-and (arithmetic-shift bb -17)(bitwise-not FILE_H)))))
+
+(define (knight-moves bb) 
+  (bitwise-xor 
+                ;; EAST
+               (bitwise-ior (bitwise-ior (bitwise-and (arithmetic-shift bb 17)  (bitwise-not FILE_H)) 
+                                         (bitwise-and (arithmetic-shift bb 10)  (bitwise-not FILE_GH))) 
+                            (bitwise-ior (bitwise-and (arithmetic-shift bb -6)  (bitwise-not FILE_GH)) 
+                                         (bitwise-and (arithmetic-shift bb -15) (bitwise-not FILE_H))))    
+                ;; WEST
+               (bitwise-ior (bitwise-ior (bitwise-and (arithmetic-shift bb 15)  (bitwise-not FILE_A)) 
+                                         (bitwise-and (arithmetic-shift bb 6)   (bitwise-not FILE_AB))) 
+                            (bitwise-ior (bitwise-and (arithmetic-shift bb -10) (bitwise-not FILE_AB)) 
+                                         (bitwise-and (arithmetic-shift bb -17) (bitwise-not FILE_A)))))) 
 
 (define test (knight-moves KNIGHT))
 (printBitBoard test)
+
+
